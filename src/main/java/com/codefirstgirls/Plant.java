6a5e33ba2c38c554398123ad37e6038a17b5a171
Jess Branch <jessbranch@hotmail.co.uk>
2026-01-15 11:19:26 +0000

    Created plant parent class

---
+++ src/main/java/com/codefirstgirls/Plant.java
@@ -0,0 +1,13 @@
+package com.codefirstgirls;
+
+
+public class Plant {
+    private String name;
+    private Lifespan lifespan;
+
+    //constructor
+    public Plant(String name, Lifespan lifespan){
+        this.name = name;
+        this.lifespan = lifespan;
+    }
+}
