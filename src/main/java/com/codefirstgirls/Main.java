457504f476f8d7c518e032ebae96a31d54b5147f
Jess Branch <jessbranch@hotmail.co.uk>
2026-01-15 10:53:06 +0000

    Changed text in main file for tomato planter

--- src/main/java/com/codefirstgirls/Main.java
+++ src/main/java/com/codefirstgirls/Main.java
@@ -7,14 +7,15 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in); // Scanner class is used to get user input
 
     public static void main(String[] args) {
-        System.out.println("WELCOME TO THE OBJECT GENERATOR");
+        System.out.println("WELCOME TO THE TOMATO PLANTER");
         System.out.println("*******************************");
-        System.out.println("PLEASE ENTER ONE OF THE FOLLOWING OBJECTS TO CREATE");
+        System.out.println("PLEASE ENTER ONE OF THE FOLLOWING TOMATOES TO PLANT");
 
         //print out your list here
+        System.out.println("Gardener's Delight,\nTumbling tom,\nSungold,\nBlack Cherry,\nGreen Zebra");
 
-        String menuChoice = scanner.nextLine();
-        System.out.println("THANKS! HOW MANY WOULD YOU LIKE TO CREATE?");
+        String tomatoChoice = scanner.nextLine();
+        System.out.println("THANKS! HOW MANY WOULD YOU LIKE TO PLANT?");
         String amountToCreate = scanner.nextLine();
 
         // create the items, and output to the user as appropriate
