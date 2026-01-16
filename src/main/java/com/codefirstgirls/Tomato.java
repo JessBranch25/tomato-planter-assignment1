0a457532a0a44a94458c6a56d11af3b621446309
Jess Branch <jessbranch@hotmail.co.uk>
2026-01-16 10:42:55 +0000

    Added methods to the tomato class

--- src/main/java/com/codefirstgirls/Tomato.java
+++ src/main/java/com/codefirstgirls/Tomato.java
@@ -13,5 +13,52 @@ public class Tomato extends Plant {
         this.tomatoType = tomatoType;
         this.fruitColour = fruitColour;
         this.taste = taste;
+
+    }
+
+    //create getters for all tomato attributes
+    public String getSpecies(){
+        return species;
+    }
+
+    public TomatoType getTomatoType(){
+        return tomatoType;
+    }
+
+    public String getFruitColour(){
+        return fruitColour;
+    }
+
+    public String getTaste(){
+        return taste;
+    }
+
+    //create method to return tomato information
+    public void printTomatoInformation(){
+        String typeString = convertTomatoTypeToString();
+
+        System.out.println(species+" tomatoes are "+typeString+" tomatoes with a "+fruitColour+" colour and "+taste+" flavour");
+    }
+
+    //needs a helper function to convert the tomato type to a string, did this using a switch statement to practice them
+    private String convertTomatoTypeToString(){
+        String tomatoTypeString = "";
+
+        switch (tomatoType){
+            case TomatoType.CHERRY:
+                tomatoTypeString = "cherry";
+                break;
+            case TomatoType.BEEF:
+                tomatoTypeString = "beef";
+                break;
+            case TomatoType.PLUM:
+                tomatoTypeString = "plum";
+                break;
+            case TomatoType.HERITAGE:
+                tomatoTypeString = "heritage";
+                break;
+        }
+
+        return tomatoTypeString;
     }
 }
