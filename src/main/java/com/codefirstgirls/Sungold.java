dfb4835ba2934803cc53b7580e01543b4c2a575e
Jess Branch <jessbranch@hotmail.co.uk>
2026-01-16 12:11:48 +0000

    Added more subclasses for other types of tomatoes

--- src/main/java/com/codefirstgirls/Sungold.java
+++ src/main/java/com/codefirstgirls/Sungold.java
@@ -2,7 +2,7 @@ package com.codefirstgirls;
 
 public class Sungold extends Tomato{
     public Sungold() {
-        super(getCuteName(), "Sungold", TomatoType.CHERRY, "orange", "sweet");
+        super(getCuteName(), "Sungold", TomatoType.CHERRY, "orange", "sweet, fruity");
     }
 }
 
